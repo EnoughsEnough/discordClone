009b15d92a7ba483df29b4680666083e53579d04
Mark Cherenov <user@MacBook-Pro-user.local>
2024-07-23 23:19:39 +0300

    add prettier, button component and setup folder setup

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+   return (
+      <div className="flex flex-col">
+         <p className="text-xl text-blue-600 text-center">Hello World!</p>
+         <Button>Click me</Button>
+      </div>
+   )
+}
